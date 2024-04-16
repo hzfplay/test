fce1564c0608be1c44ac9264f0f3a92566a0ff50
AW-20230912NIOG\Administrator <978223208@qq.com>
2024-04-16 17:00:53 +0800

    111

---
+++ firstBuild/firstBuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstBuildApp.swift
+//  firstBuild
+//
+//  Created by AW on 2024/4/16.
+//
+
+import SwiftUI
+
+@main
+struct firstBuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
